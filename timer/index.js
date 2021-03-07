3230735d565800368376d49be1080903a599707c
rmanaem <rmanaem@protonmail.ch>
2021-03-07 18:55:40 -0500

    Added a Tray

--- timer/index.js
+++ timer/index.js
@@ -1,10 +1,20 @@
+const path = require('path');
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        height: 550,
+        width: 350,
+        frame: false,
+        resizable: false
+    });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    new Tray(iconPath);
 })
